61e465001a88a64def14158672bc9abd088467f8
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2021-05-17 22:32:45 +0200

    Identity added
    Identity added
    
    Co-Authored-By: Mateusz Paszko <73493587+nieswietymikolaj@users.noreply.github.com>

--- Virtual_School_Register/Virtual_School_Register/Data/ApplicationDbContext.cs
+++ Virtual_School_Register/Virtual_School_Register/Data/ApplicationDbContext.cs
@@ -3,14 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Virtual_School_Register.Models;
 
 namespace Virtual_School_Register.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
     }
 }
---
+++ Virtual_School_Register/Virtual_School_Register/Models/User.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Virtual_School_Register.Models
+{
+    public class User : IdentityUser
+    {
+        [Display(Name = "Imię")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Imię musi mieć od 1 do 20 znaków!")]
+        public string Name { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Nazwisko musi mieć od 1 do 20 znaków!")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Płeć")]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "Płeć musi składać się z jednego znaku")]
+        public string Sex { get; set; }
+
+        [Display(Name = "Data urodzenia")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Adres")]
+        [MinLength(1, ErrorMessage = "Opis nie może być pusty!")]
+        public string Adress { get; set; }
+
+        [Display(Name = "Rodzic")]
+        public int? ParentId { get; set; }
+
+        //[Display(Name = "Typ użytkownika")]
+        //[Required]
+        //public string Type { get; set; }
+
+        //public virtual ICollection<Annoucement> Annoucements { get; set; }
+
+        //public virtual ICollection<Message> Messages { get; set; }
+
+        //[Display(Name = "Klasa ID")]
+        //public int? ClassId { get; set; }
+
+        //[Display(Name = "Klasa")]
+        //public virtual Class Class { get; set; }
+
+        //public virtual ICollection<StudentGradeSubject> StudentsGradesSubjects { get; set; }
+
+        //public virtual ICollection<ClassSubjectTeacher> ClassesSubjectsTeachers { get; set; }
+    }
+}
