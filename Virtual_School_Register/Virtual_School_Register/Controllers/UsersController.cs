745c99d183a94bf6b516c58e6538e58206ccf434
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2021-05-18 17:45:01 +0200

    User CRUD added
    User CRUD added

--- Virtual_School_Register/Virtual_School_Register/Controllers/UsersController.cs
+++ Virtual_School_Register/Virtual_School_Register/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Virtual_School_Register.Data;
+using Virtual_School_Register.Models;
 
 namespace Virtual_School_Register.Controllers
 {
@@ -18,13 +20,140 @@ namespace Virtual_School_Register.Controllers
             _context = context;
         }
 
+        // GET: Users
         public async Task<IActionResult> Index()
         {
             var myDbContext = _context.Users.Include(u => u.Class);
-            
+
             return View(await myDbContext.ToListAsync());
         }
 
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var user = await _context.Users
+                .Include(u => u.Class)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            ViewData["ClassId"] = new SelectList(_context.Class, "ClassId", "Name");
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> Create([Bind("Id,UserName,Password,Name,Surname,Sex,Email,PhoneNumber,BirthDate,Adress,ParentId,Type,ClassId")] User user)
+        public async Task<IActionResult> Create([Bind("Id,UserName,Name,Surname,Sex,Email,PhoneNumber,BirthDate,Adress,ParentId,ClassId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClassId"] = new SelectList(_context.Class, "ClassId", "Name", user.ClassId);
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClassId"] = new SelectList(_context.Class, "ClassId", "Name", user.ClassId);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,Name,Surname,Sex,Email,PhoneNumber,BirthDate,Adress,ParentId,ClassId")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClassId"] = new SelectList(_context.Class, "ClassId", "Name", user.ClassId);
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Class)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(string id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
     }
 }
