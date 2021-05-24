6b256ce2700c6d5131cad7886c89745bc3b9f5cc
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2021-05-24 22:25:14 +0200

    Added the ability to delete messages
    Added the ability to delete messages

---
+++ Virtual_School_Register/Virtual_School_Register/Data/Migrations/20210524183320_MessageBoolVariablesAdded.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Virtual_School_Register.Data.Migrations
+{
+    public partial class MessageBoolVariablesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRecipientDeleted",
+                table: "Message",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSenderDeleted",
+                table: "Message",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRecipientDeleted",
+                table: "Message");
+
+            migrationBuilder.DropColumn(
+                name: "IsSenderDeleted",
+                table: "Message");
+        }
+    }
+}
