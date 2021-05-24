c3a13b1575023c9dfeccf1ac255d680c7e4c6e9b
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2021-05-24 20:30:48 +0200

    Messages added
    Messages added

---
+++ Virtual_School_Register/Virtual_School_Register/Data/Migrations/20210524170155_MessageModelUpdated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Virtual_School_Register.Data.Migrations
+{
+    public partial class MessageModelUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RecipientId",
+                table: "Message",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipientId",
+                table: "Message",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
