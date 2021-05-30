8de2aeae853df5890e23ef46d247d2cfbfc17b0e
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2021-05-30 18:31:15 +0200

    Added sending of announcements by teachers to parents of a given class, both via email and via the website
    Added sending of announcements by teachers to parents of a given class, both via email and via the website

---
+++ Virtual_School_Register/Virtual_School_Register/EmailConfig/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Virtual_School_Register.EmailConfig
+{
+    public interface IEmailSender
+    {
+        void SendEmail(MyMessage myMessage);
+    }
+}
