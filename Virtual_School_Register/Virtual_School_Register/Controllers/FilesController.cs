b4b3e5619f79a60cd4040c8f7a5308b3a69678a4
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2021-05-31 13:13:34 +0200

    Added adding and downloading files
    Added adding and downloading files

--- Virtual_School_Register/Virtual_School_Register/Controllers/FilesController.cs
+++ Virtual_School_Register/Virtual_School_Register/Controllers/FilesController.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Virtual_School_Register.Data;
 using Virtual_School_Register.Models;
+using Virtual_School_Register.ViewModels;
 
 namespace Virtual_School_Register.Controllers
 {
     public class FilesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public FilesController(ApplicationDbContext context)
+        public FilesController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: Files
@@ -24,6 +32,22 @@ namespace Virtual_School_Register.Controllers
         {
             var applicationDbContext = _context.File.Include(f => f.Subject);
             return View(await applicationDbContext.ToListAsync());
+        }
+
+        public ActionResult DownloadFile(int fileId)
+        {
+            var file = _context.File.Find(fileId);
+
+            /*var cd = new ContentDisposition
+            {
+                FileName = file.Name,
+                Inline = false,
+            };*/
+            //Response.Headers.Add("Content-Disposition", file.Name);
+
+            string type = "application/" + file.FileType.ToString();
+
+            return File(file.DataFiles, type, file.Name);
         }
 
         // GET: Files/Details/5
@@ -53,14 +77,36 @@ namespace Virtual_School_Register.Controllers
         }
 
         // POST: Files/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FileId,Name,SubjectId")] File file)
+        public async Task<IActionResult> Create(Models.File file, IFormFile files)
         {
             if (ModelState.IsValid)
             {
+                if (files != null)
+                {
+                    if (files.Length > 0)
+                    {
+                        //Getting FileName
+                        var fileName = Path.GetFileName(files.FileName);
+                        //Getting file Extension
+                        var fileExtension = Path.GetExtension(fileName);
+                        // concatenating  FileName + FileExtension
+                        //var newFileName = String.Concat(Convert.ToString(Guid.NewGuid()), fileExtension);
+
+                        /*file.Name = newFileName;*/
+                        file.Name = fileName;
+                        file.FileType = fileExtension;
+                        file.Date = DateTime.Now;
+
+                        using (var target = new MemoryStream())
+                        {
+                            files.CopyTo(target);
+                            file.DataFiles = target.ToArray();
+                        }
+                    }
+                }
+                
                 _context.Add(file);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -89,7 +135,7 @@ namespace Virtual_School_Register.Controllers
         // POST: Files/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("FileId,Name,SubjectId")] File file)
+        public async Task<IActionResult> Edit(int id, [Bind("FileId,Name,SubjectId")] Models.File file)
         {
             if (id != file.FileId)
             {
---
+++ Virtual_School_Register/Virtual_School_Register/Data/Migrations/20210531091523_FilesParametersUpdated.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Virtual_School_Register.Data.Migrations
+{
+    public partial class FilesParametersUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "DataFiles",
+                table: "File",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "File",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileType",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UserDetailsViewModel",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sex = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Adress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ParentId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClassId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDetailsViewModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserDetailsViewModel_Class_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Class",
+                        principalColumn: "ClassId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDetailsViewModel_ClassId",
+                table: "UserDetailsViewModel",
+                column: "ClassId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDetailsViewModel");
+
+            migrationBuilder.DropColumn(
+                name: "DataFiles",
+                table: "File");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "File");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "File");
+
+            migrationBuilder.DropColumn(
+                name: "FileType",
+                table: "File");
+        }
+    }
+}
